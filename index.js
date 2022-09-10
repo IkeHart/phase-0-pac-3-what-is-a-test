e53cda927e5b5b5c5cfab3910a3869befd6aab86
Ike Reed <ikereed@protonmail.com>
2022-09-10 14:16:42 -0500

    commit tree bf0cf89e63c5c8ce048ba573b4d55b823eefe830

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
+const name = "Noel";
 const height = 74;
-const message = `${name} is ${height} inches tall`;
+const message = `${name} is ${height} centimeters tall`;
 
 module.exports = {name, height, message};
+console.log(message);
